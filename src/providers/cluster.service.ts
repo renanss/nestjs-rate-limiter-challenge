d85ae84914e645622c044504b5387453d5da87e9
Renan Soares <renanss@gmail.com>
2023-01-25 08:36:12 -0300

    Added code comment to explain concurrency decisions at code-level.

--- src/providers/cluster.service.ts
+++ src/providers/cluster.service.ts
@@ -4,7 +4,12 @@ import { Injectable } from '@nestjs/common';
 
 const numCPUs = os.cpus().length;
 const cluster = clusterImport as unknown as clusterImport.Cluster;
-//Very basic clusterization, so we can test concurrency as required by the code challenge.
+/*Very basic clusterization, so we can test concurrency as required by the code challenge.
+	Other ways to achieve concurrency --at code level, may include:
+		- Using a message queue such as Bull(officially recommended by Nest.js) to distribute the load.
+		- Using child processes. The downside is that we still need to heavily rely on the OS to distribute the load, and uncontrolled child processes may cause memory leaks or hang the server.
+*/
+
 @Injectable()
 export class AppClusterService {
   static clusterize(callback: any): void {
